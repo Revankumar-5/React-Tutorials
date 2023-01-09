dd52f960baa92020c92bdfd0366ffa71e24afd0c
Revan <revankumar.p@arkinfotec.com>
2023-01-09 12:08:41 +0530

    #5 UpdateEffect Bug Fixes

--- src/4.useUpdateEffect/UseUpdateEffect.js
+++ src/4.useUpdateEffect/UseUpdateEffect.js
@@ -2,16 +2,27 @@ import { useEffect, useRef } from "react";
 
 export default function UseUpdateEffect(callback, dependencies) {
 
- const firstRendered=useRef(true)
- console.log(firstRendered.current);
- console.log(dependencies);
+ const firstRendered=useRef(0)
 
   useEffect(() => {
-    if (firstRendered.current) {
-        firstRendered.current=false
+    if (firstRendered.current <2) {
+        firstRendered.current=firstRendered.current=firstRendered.current+1;
       return
     }
 
         return callback();
   }, dependencies);
 }
+
+// import { useEffect, useRef} from "react"
+// export default function useUpdateEffect (callback, dependencies) {
+// const firstRenderRef = useRef(true)
+
+// useEffect (() => {
+// if (firstRenderRef.current) {
+// firstRenderRef.current = false
+// return
+// }
+// return callback()
+// }, dependencies)
+// }
--- src/4.useUpdateEffect/UseUpdateEffectIndex.js
+++ src/4.useUpdateEffect/UseUpdateEffectIndex.js
@@ -14,3 +14,15 @@ return(
 </div>
 ) 
 }
+
+// import { useState } from "react"
+// import useUpdateEffect from "./UseUpdateEffect"
+// export default function UpdateEffectComponent() {
+// const [count, setCount] = useState(10)
+
+// useUpdateEffect (() => alert(count), [count])
+// return (
+// <div>
+// <div> {count}</div>
+// <button onClick={() => setCount(c => c + 1)}>Increment</button>
+// </div>)}
